8f8efa7cd65aed7fc20f9c8cc1914e19b05d6d12
Shelby S <3541579+sillyfatcat@users.noreply.github.com>
2021-06-07 15:19:12 -0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -5,3 +5,7 @@ logger -s "Wallpaper path: $wallpaper_path"
 logger -s "Setting wallpaper"
 osascript -e 'tell application "Finder" to set desktop picture to POSIX file "'$wallpaper_path'"'
 logger -s "Wallpaper set"
+logger -s "Setting Git Branch parser"
+git_branch_path="$(pwd)/parsed_git_branch.sh"
+cat "$git_branch_path" >> ~/.bash_profile
+logger -s "Git Branch parser set"
